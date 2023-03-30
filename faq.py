999a135c0d8101ece9a5018a87022dde17a9cf59
Laura Salas <laurasalas96@live.com>
2023-03-30 18:28:45 -0400

    add help message

--- faq.py
+++ faq.py
@@ -19,7 +19,7 @@ async def validate_faq_add(message, client, db_channel):
     maybe_response = await get_response(message.content.strip().split(" ")[0], client, db_channel, ignore_last_comment=True)
     if maybe_response != "":
         await db_channel.send(
-            "ERROR: id `%s` already exists in this channel; it points to table with value ```%s```" % 
+            "ERROR: id `%s` already exists in this channel; it points to table with value ```%s```. **Please delete the message you just sent to avoid conflicting database IDs!**" % 
             (
                 message.content.strip().split(" ")[0], 
                 maybe_response
@@ -38,3 +38,19 @@ async def reply_to_question(message, client, db_channel):
             response = await get_response(command, client,  db_channel) 
             if response.strip() != "":
                 await message.channel.send(response.strip())
+            else:
+                await message.channel.send("Sorry, I did not find an answer for id `%s`. To view available ids, mention me again and type `ids`" % command)
+
+
+async def get_all_ids(message, client, db_channel):
+    history = await retrieve_n_messages_from_chat_history(500, client, db_channel)
+    history.reverse()
+    ids = []
+    for msg in history:
+        print(msg)
+        if "!" in msg.strip()[0:2]:
+            ids.append(msg.strip().split(" ")[0]) 
+    history_as_string = ""
+    for id_ in ids:
+        history_as_string += "\n- `%s`" % id_
+    await message.channel.send("Here are the ids in my database: \n- %s" % history_as_string)
--- ginette.py
+++ ginette.py
@@ -34,6 +34,14 @@ client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix="$", description=description, intents=intents)
 recentEmoji = [""]
 
+HELP_PROMPT = """
+Hi! I'm Ginette Savard, AÉDIROUM's helper bot 🤖.
+Here's some things you can do with me:
+- chat with me on the `#chatter` channel, powered by chat gpt.
+- ask me questions in my database of questions, by mentioning me and following the message with the id of the question you'd like answer. For instance: `@Ginette !coursdete`. 
+- ask me what are the ids in my database of questions by mentioning me and following the message with `!ids`.
+-  If you want to learn more about me, you can ask me `!help`.
+"""
 
 @client.event
 async def on_raw_reaction_add(reaction):
@@ -52,14 +60,19 @@ async def on_message(message):
         # elif message.channel.id == client.get_channel(FAQ_CHANNEL).id:
         #     await reply_to_question(message, client, client.get_channel(FAQ_DB))
         elif client.user in message.mentions:
-            await reply_to_question(message, client, client.get_channel(FAQ_DB))
+            if "!help" in message.content:
+                await message.channel.send(HELP_PROMPT)
+            elif "!ids" in message.content:
+                await get_all_ids(message, client, client.get_channel(FAQ_DB))
+            else: 
+                await reply_to_question(message, client, client.get_channel(FAQ_DB))
         # new faq database addition
         elif message.channel.id == client.get_channel(FAQ_DB).id:
             await validate_faq_add(message, client, client.get_channel(FAQ_DB))
 
 @client.event
 async def on_ready(): 
-    initialize_ginette(client.get_channel(CHATTER_CHANNEL))
+    await initialize_ginette(client.get_channel(CHATTER_CHANNEL))
     print('We have logged in as {0.user}'.format(client))
     
 client.run(TOKEN)
